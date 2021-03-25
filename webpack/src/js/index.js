285d8c0888aeff5b11f832b3721cc05a7ffa8d1f
zhangkai <and0429@gmail.com>
2021-03-25 23:54:50 +0800

    d

---
+++ webpack/src/js/index.js
@@ -0,0 +1,8 @@
+import 'reset-css';
+import '../css/index.less';
+
+function add(x, y) {
+    return x + y;
+}
+
+console.log(add(3, 5))
---
+++ webpack/webpack.config.js
@@ -0,0 +1,33 @@
+const { resolve } = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'js/build.js',
+        path: resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }, {
+            test: /\.less$/,
+            use: ['style-loader', 'css-loader', 'less-loader']
+        }]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+
+    mode: 'development',
+
+    devServer: {
+        port: 8080
+    }
+
+}
