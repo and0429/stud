bf7dd76f41e15753e63c34c7ae337dedada4eb67
zhangkai <and0429@gmail.com>
2021-03-28 22:40:56 +0800

    js 兼容性

--- webpack/src/js/index.js
+++ webpack/src/js/index.js
@@ -1,14 +1,10 @@
 import 'reset-css';
-
 import '../css/index.less';
-
 import './check';
-
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function add(x, y) {
   return x + y;
 }
 
-// eslint-disable-next-line
-console.log(add(3, 5));
+add(1, 3);
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -13,84 +13,117 @@ module.exports = {
         path: resolve(__dirname, 'build')
     },
     module: {
-        rules: [{ // js语法检查 // 使用airbnb进行语法检查 ,使用  npm i eslint-config-airbnb-base
-            //  "extends": "airbnb-base" 可以配置在package.json 也可以在 .eslintrc 中。
-            test: /\.js/i,
-            exclude: /node_modules/,
-            use: [{
-                loader: 'eslint-loader',
+        rules: [
+            { // js 兼容性处理
+                test: /\.js/i,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env', // 预设处理
+                                {
+                                    useBuiltIns: 'usage', // 使用corejs按需处理
+                                    corejs: {
+                                        version: 3
+                                    },
+                                    targets: { // 浏览器需求
+                                        chrome: '60',
+                                        ie: '9',
+                                        firefox: '50',
+                                        edge: '17',
+                                        safari: '10'
+                                    }
+                                }
+                            ]
+                        ]
+                    }
+                }, {
+                    loader: 'eslint-loader',
+                    options: {
+                        fix: true, // 自动修复不合格的语法
+                    }
+                }]
+            },
+            // { // js语法检查 // 使用airbnb进行语法检查 ,使用  npm i eslint-config-airbnb-base
+            //     //  "extends": "airbnb-base" 可以配置在package.json 也可以在 .eslintrc 中。
+            //     test: /\.js/i,
+            //     exclude: /node_modules/,
+            //     use: [{
+            //         loader: 'eslint-loader',
+            //         options: {
+            //             fix: true, // 自动修复不合格的语法
+            //         }
+            //     }]
+            // },
+            {
+                test: /\.css$/i,
+                use: [
+                    // 'style-loader',  // 创建style标签，将css插入进去，
+                    {// 提出CSS为单独文件
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../'
+                        }
+                    },
+                    'css-loader', // 将css弄到js中
+                    {   // css 兼容性处理
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: ['postcss-preset-env'] // browserslist
+                            }
+                        }
+
+                    }
+                ]
+            }, {
+                test: /\.less$/i,
+                use: [
+                    // 'style-loader', 
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../'
+                        }
+                    },
+                    'css-loader',
+                    {   // css 兼容性处理
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: ['postcss-preset-env']
+                            }
+                        }
+
+                    },
+                    'less-loader'
+                ]
+            }, { // 处理图片资源，limit以下大小的图片使用base64
+                test: /\.(png|jpg|gif|svg|jpeg)/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: '[hash:10].[ext]',
+                            outputPath: 'static',
+                            esModule: false
+                        }
+                    }
+                ]
+            }, { // 处理静态资源
+                exclude: /\.(css|less|html|js|png|jpg|gif|svg|jpeg)$/i,
+                loader: 'file-loader',
                 options: {
-                    fix: true, // 自动修复不合格的语法
+                    name: '[hash:10].[ext]',
+                    outputPath: 'static'
                 }
+            }, { // 处理html中的图片
+                test: /\.html$/i,
+                loader: 'html-loader'
             }]
-        },
-        {
-            test: /\.css$/i,
-            use: [
-                // 'style-loader',  // 创建style标签，将css插入进去，
-                {// 提出CSS为单独文件
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                        publicPath: '../'
-                    }
-                },
-                'css-loader', // 将css弄到js中
-                {   // css 兼容性处理
-                    loader: 'postcss-loader',
-                    options: {
-                        postcssOptions: {
-                            plugins: ['postcss-preset-env'] // browserslist
-                        }
-                    }
-
-                }
-            ]
-        }, {
-            test: /\.less$/i,
-            use: [
-                // 'style-loader', 
-                {
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                        publicPath: '../'
-                    }
-                },
-                'css-loader',
-                {   // css 兼容性处理
-                    loader: 'postcss-loader',
-                    options: {
-                        postcssOptions: {
-                            plugins: ['postcss-preset-env']
-                        }
-                    }
-
-                },
-                'less-loader'
-            ]
-        }, { // 处理图片资源，limit以下大小的图片使用base64
-            test: /\.(png|jpg|gif|svg|jpeg)/i,
-            use: [
-                {
-                    loader: 'url-loader',
-                    options: {
-                        limit: 8192,
-                        name: '[hash:10].[ext]',
-                        outputPath: 'static',
-                        esModule: false
-                    }
-                }
-            ]
-        }, { // 处理静态资源
-            exclude: /\.(css|less|html|js|png|jpg|gif|svg|jpeg)$/i,
-            loader: 'file-loader',
-            options: {
-                name: '[hash:10].[ext]',
-                outputPath: 'static'
-            }
-        }, { // 处理html中的图片
-            test: /\.html$/i,
-            loader: 'html-loader'
-        }]
     },
 
     plugins: [
