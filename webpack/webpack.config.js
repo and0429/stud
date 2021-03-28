0a589270df516e03d9fef313abb5805ab2a2a4b5
zhangkai <and0429@gmail.com>
2021-03-28 19:44:57 +0800

    压缩css代码

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,10 @@
 const { resolve } = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+process.env.NODE_ENV = 'production' // 设置环境变量，用于postcss
 
 module.exports = {
     entry: './src/js/index.js',
@@ -11,24 +15,62 @@ module.exports = {
     module: {
         rules: [{
             test: /\.css$/i,
-            use: ['style-loader', 'css-loader']
+            use: [
+                // 'style-loader',  // 创建style标签，将css插入进去，
+                {// 提出CSS为单独文件
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: '../'
+                    }
+                },
+                'css-loader', // 将css弄到js中
+                {   // css 兼容性处理
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            plugins: ['postcss-preset-env'] // browserslist
+                        }
+                    }
+
+                }
+            ]
         }, {
             test: /\.less$/i,
-            use: ['style-loader', 'css-loader', 'less-loader']
+            use: [
+                // 'style-loader', 
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: '../'
+                    }
+                },
+                'css-loader',
+                {   // css 兼容性处理
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            plugins: ['postcss-preset-env']
+                        }
+                    }
+
+                },
+                'less-loader'
+            ]
         }, { // 处理图片资源，limit以下大小的图片使用base64
-            test: /\.(png|jpg|gif|svg)/i,
+            test: /\.(png|jpg|gif|svg|jpeg)/i,
             use: [
                 {
                     loader: 'url-loader',
                     options: {
                         limit: 8192,
                         name: '[hash:10].[ext]',
-                        outputPath: 'static'
+                        outputPath: 'static',
+                        esModule: false
                     }
                 }
             ]
         }, { // 处理静态资源
-            exclude: /\.(css|less|html|js|png|jpg|gif|svg)$/i,
+            exclude: /\.(css|less|html|js|png|jpg|gif|svg|jpeg)$/i,
             loader: 'file-loader',
             options: {
                 name: '[hash:10].[ext]',
@@ -41,9 +83,13 @@ module.exports = {
     },
 
     plugins: [
-        new HtmlWebpackPlugin({
+        new HtmlWebpackPlugin({ // html模版到处
             template: './src/index.html'
-        })
+        }),
+        new MiniCssExtractPlugin({ // 提取css到单独文件
+            filename: 'css/[hash:10].css'
+        }),
+        new OptimizeCssAssetsPlugin() // 压缩css
     ],
 
     mode: 'development',
